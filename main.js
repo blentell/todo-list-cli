cc75f499af5b0080aeeb668bd3e414709ba605bd
Brion Lentell <brion@Brions-MacBook-Pro.local>
2021-06-14 15:24:03 -0400

    Update working add items and update completion

--- main.js
+++ main.js
@@ -8,19 +8,39 @@ const completedItemList = [];
 let selector = "";
 
 while (selector !== 3) {
-    console.log(toDoItemList);
+    console.log(`You have ${toDoItemList.length} item(s) on your list`)
+    console.log("");
+    for (i = 0; i < toDoItemList.length; i++){
+        console.log(`${i + 1}. ${completedItemList[i]} ${toDoItemList[i]}`);
+    }
+    console.log("");
+    console.log("~ Select an action ~");
     console.log(toDoItem);
     console.log(completeToDo);
     console.log(end);
     selector = Number(prompt('> '));
+
     
     if (selector === 1) {
         let item = prompt("Add your item: ");
         toDoItemList.push(item);
+        completedItemList.push('[Incomplete]');
     }
     if (selector === 2) {
-        completedItemList = prompt("Which item should be completed? ");
+        const update = prompt("Which item should be completed? ");
+        let found = false;
+        for (i = 0; i < toDoItemList.length; i++) {
+            if (update === toDoItemList[i]) {
+                completedItemList[i] = '[Complete]';
+                found = true;
+            }
+            
+        }
+        if (found == false) {
+            console.log("That is not a valid item!");
+        }
     }
-}
+    }
+
 
 
