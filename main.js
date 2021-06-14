1a316090a62af1d197c9325bb681b43138cba895
Brion Lentell <brion@Brions-MacBook-Pro.local>
2021-06-14 12:58:57 -0400

    Update with adding items to array working

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ const prompt = require('prompt-sync')();
 const toDoItem = '[1] Create a to-do item ';
 const completeToDo = '[2] Complete a to-do item ';
 const end = '[3] End to-do list ';
-console.log(toDoItem);
-console.log(completeToDo);
-console.log(end);
-let selector = Number(prompt('> '));
+const toDoItemList = [];
+const completedItemList = [];
+let selector = "";
 
 while (selector !== 3) {
+    console.log(toDoItemList);
     console.log(toDoItem);
     console.log(completeToDo);
     console.log(end);
-    console.log(selector);
-
+    selector = Number(prompt('> '));
+    
+    if (selector === 1) {
+        let item = prompt("Add your item: ");
+        toDoItemList.push(item);
+    }
+    if (selector === 2) {
+        completedItemList = prompt("Which item should be completed? ");
+    }
 }
 
 
