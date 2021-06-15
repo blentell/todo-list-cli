43d482e827b416b4c84d99b4728ceef450d91fcd
blentell <blentell@aol.com>
2021-06-15 11:46:32 -0400

    Update to functions

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 const prompt = require('prompt-sync')();
-
+// Establish variables
 const toDoItem = '[1] Create a to-do item ';
 const completeToDo = '[2] Complete a to-do item ';
 const end = '[3] End to-do list ';
+// Create array to contain the to-do list
 const toDoItemList = [];
+// Create array to contain the completed status
 const completedItemList = [];
+// Establish the selection method
 let selector = "";
 
-while (selector !== 3) {
+function addToList() {
+        let item = prompt("Add your item: ");
+        toDoItemList.push(item);
+        completedItemList.push('[Incomplete]'); 
+}
+
+function makeComplete() {
+        const update = prompt("Which item should be completed? ");
+        let found = false;  // Had to create a boolean to not print out the 'invalid item'
+        for (i = 0; i < toDoItemList.length; i++) {
+            if (update === toDoItemList[i]) {
+                completedItemList[i] = '[Complete]';
+                found = true;
+            }        
+        }
+        if (found == false) {  // This tells the user that the item they want to update does not exist
+            console.log("That is not a valid item!");
+        }   
+}
+
+while (selector !== 3) {  // This will run until the user exits with a '3'
     console.log(`You have ${toDoItemList.length} item(s) on your list`)
     console.log("");
-    for (i = 0; i < toDoItemList.length; i++){
+    for (i = 0; i < toDoItemList.length; i++){  // iterate through to count the number of items and print out the number
         console.log(`${i + 1}. ${completedItemList[i]} ${toDoItemList[i]}`);
     }
     console.log("");
@@ -21,24 +44,11 @@ while (selector !== 3) {
     selector = Number(prompt('> '));
 
     
-    if (selector === 1) {
-        let item = prompt("Add your item: ");
-        toDoItemList.push(item);
-        completedItemList.push('[Incomplete]');
+    if (selector === 1) {  // This is where we will add items to the list and auto assign 'Incomplete' to them
+        addToList();
     }
-    if (selector === 2) {
-        const update = prompt("Which item should be completed? ");
-        let found = false;
-        for (i = 0; i < toDoItemList.length; i++) {
-            if (update === toDoItemList[i]) {
-                completedItemList[i] = '[Complete]';
-                found = true;
-            }
-            
-        }
-        if (found == false) {
-            console.log("That is not a valid item!");
-        }
+    if (selector === 2) {  // This is where we can select something to be marked as 'Complete'
+        makeComplete();
     }
     }
 
