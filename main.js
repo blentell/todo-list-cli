17b7e2a75cec7ea7784ace8ad95b79caa48a3a9e
blentell <blentell@aol.com>
2021-06-15 12:12:07 -0400

    Update with ability to add, remove, edit, and change status of items on the list

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const prompt = require('prompt-sync')();
 // Establish variables
 const toDoItem = '[1] Create a to-do item ';
 const completeToDo = '[2] Complete a to-do item ';
-const end = '[3] End to-do list ';
+const editItem = '[3] Edit an item';
+const removeItem = '[4] Remove an item';
+const end = '[5] End to-do list ';
 // Create array to contain the to-do list
 const toDoItemList = [];
 // Create array to contain the completed status
@@ -20,17 +22,49 @@ function makeComplete() {
         const update = prompt("Which item should be completed? ");
         let found = false;  // Had to create a boolean to not print out the 'invalid item'
         for (i = 0; i < toDoItemList.length; i++) {
-            if (update === toDoItemList[i]) {
+            if (update === toDoItemList[i] && completedItemList[i] === '[Incomplete]') {
                 completedItemList[i] = '[Complete]';
                 found = true;
-            }        
+            }
+            else {
+                completedItemList[i] = '[Incomplete]';
+            }
         }
         if (found == false) {  // This tells the user that the item they want to update does not exist
             console.log("That is not a valid item!");
         }   
 }
 
-while (selector !== 3) {  // This will run until the user exits with a '3'
+function editItemOnList() { // This allows us to edit an item on the list
+    const edit = prompt("Which item should be edited? ");
+    let found = false;
+    for (i = 0; i < toDoItemList.length; i++){
+        if (edit === toDoItemList[i]) {
+            const edit2 = prompt(`Change ${edit} to what? `);
+            toDoItemList.splice(i, 1, edit2);
+            found = true;
+        }
+    }
+    if (found === false) {
+        console.log("That is not a valid item!");
+    }
+}
+
+function removeItemFromList() {  // This allows us to remove an item from the list
+    const remove = prompt("Which item should be removed? ");
+    let found = false;
+    for (i = 0; i < toDoItemList.length; i++){
+        if (remove === toDoItemList[i]) {
+            toDoItemList.splice(i, 1);
+            found = true;
+        }
+    }
+    if (found === false) {
+        console.log("That is not a valid item!");
+    }
+}
+
+while (selector !== 5) {  // This will run until the user exits with a '5'
     console.log(`You have ${toDoItemList.length} item(s) on your list`)
     console.log("");
     for (i = 0; i < toDoItemList.length; i++){  // iterate through to count the number of items and print out the number
@@ -40,6 +74,8 @@ while (selector !== 3) {  // This will run until the user exits with a '3'
     console.log("~ Select an action ~");
     console.log(toDoItem);
     console.log(completeToDo);
+    console.log(editItem);
+    console.log(removeItem);
     console.log(end);
     selector = Number(prompt('> '));
 
@@ -49,6 +85,12 @@ while (selector !== 3) {  // This will run until the user exits with a '3'
     }
     if (selector === 2) {  // This is where we can select something to be marked as 'Complete'
         makeComplete();
+    }
+    if (selector === 3) { // This is where we can select something to be edited
+        editItemOnList();
+    }
+    if (selector === 4) {  // This is where we can select something to be removed
+        removeItemFromList();
     }
     }
 
